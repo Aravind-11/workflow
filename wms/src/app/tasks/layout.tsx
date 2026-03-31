5e678a3bb52033d47fe8a2c4c38563415a251e90
ARAVINDHAN T <arvindhant01@gmail.com>
2026-03-31 13:31:40 -0700

    Revert "Merge pull request #13 from Aravind-11/feat/threejs-visualizer-bot-control"
    This reverts commit 63dfca14f6c7b10ef28cfe979d792426c17dd1b1, reversing
    changes made to 0a23fc11807b05322ec8dd7af589621ae44851dc.

--- wms/src/app/tasks/layout.tsx
+++ wms/src/app/tasks/layout.tsx
@@ -1,13 +1,7 @@
 import { P } from "@/lib/auth/permissions";
 import { requirePermission } from "@/lib/auth/session";
-import { TaskSubNav } from "@/components/tasks/task-subnav";
 
 export default async function TasksLayout({ children }: { children: React.ReactNode }) {
   await requirePermission(P.tasks.manage);
-  return (
-    <div className="space-y-4">
-      <TaskSubNav />
-      {children}
-    </div>
-  );
+  return <>{children}</>;
 }
